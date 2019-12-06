fd7d0dd374a0dcbb625869576b45d1f19bbaeb17
mak1k1 <martin.cehelsky.ja@gmail.com>
2019-12-06 19:15:57 +0100

    Implement form validation and store the question

---
+++ resources/views/layouts/_messages.blade.php
@@ -0,0 +1,8 @@
+@if (session('success'))
+    <div class="alert alert-success alert-dismissible fade show">
+        <strong>Success!</strong>{{ session('success') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span>&times;</span>
+        </button>
+    </div>
+@endif
