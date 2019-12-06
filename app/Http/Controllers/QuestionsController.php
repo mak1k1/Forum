1572164cb0f606b0cc1415b126941b77c3454aef
mak1k1 <martin.cehelsky.ja@gmail.com>
2019-12-06 15:37:37 +0100

    Build Question form

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -26,7 +26,9 @@ class QuestionsController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question();
+
+        return view('questions.create', compact($question));
     }
 
     /**
@@ -37,7 +39,7 @@ class QuestionsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
