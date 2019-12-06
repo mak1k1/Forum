d4de2308f077146d810062f41f46b18e51656d1d
mak1k1 <martin.cehelsky.ja@gmail.com>
2019-12-07 00:45:08 +0100

    Update best_answer_id on questions table when an answer removed using Eloquent event

--- app/Answer.php
+++ app/Answer.php
@@ -24,6 +24,10 @@ class Answer extends Model
         return $this->created_at->diffForHumans();
     }
 
+    public function getStatusAttribute(){
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
+
     public static function boot(){
         parent::boot();
 
@@ -32,7 +36,12 @@ class Answer extends Model
         });
 
         static::deleted(function($answer){
-            $answer->question->increment('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if($question->best_answer_id === $answer->id){
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
     }
 }
