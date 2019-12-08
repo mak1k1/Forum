cf8a5b8e7184237f234ef40887999b81065578b1
mak1k1 <martin.cehelsky.ja@gmail.com>
2019-12-08 21:33:33 +0100

    Implemented voting of the answers

--- app/User.php
+++ app/User.php
@@ -82,4 +82,18 @@ class User extends Authenticatable
         $question->votes_count = $upVotes + $downVotes;
         $question->save();
     }
+
+    public function voteAnswer(Answer $answer,$vote){
+        $voteAnswers = $this->voteAnswers();
+        if ($voteAnswers->where('votable_id', $answer->id)->exists()) {
+            $voteAnswers->updateExistingPivot($answer, ['vote' => $vote]);
+        }
+        else{
+            $voteAnswers->attach($answer, ['vote' => $vote]);
+        }
+        $downVotes = $answer->downVotes()->sum('vote');
+        $upVotes = $answer->upVotes()->sum('vote');
+        $answer->votes_count = $upVotes + $downVotes;
+        $answer->save();
+    }
 }
